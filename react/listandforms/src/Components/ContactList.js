a5a7c6b6a39f0949177edd2be9c37c73347c54ad
Mrangta <mrangta010@gmail.com>
2019-04-11 11:16:22 +0300

    Added Edit functionality

--- react/listandforms/src/Components/ContactList.js
+++ react/listandforms/src/Components/ContactList.js
@@ -1,67 +1,145 @@
-import React, { Component } from 'react';
-import {Table,Button} from 'semantic-ui-react';
+import React from 'react';
+import {Table} from 'semantic-ui-react';
+import {NormalRow} from './NormalRow';
+import {RemoveRow} from './RemoveRow';
+import EditRow from './EditRow';
+export default class ContactList extends React.Component {
 
-export default class ContactList extends Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      removeId : 0
-    }
-  }
-  remove = (id) =>{
-    let tempId = parseInt(id,10);
-    if(this.state.removeId===0){
-      this.setState({
-        removeId:tempId
-      })
-      return;
-    }
-    if(tempId===this.state.removeId){
-    this.props.remove(id);
-    }
-    this.setState({
-      removeId:0
-    })
-  
-  }
-  render(){
-    if(this.props.list.length === 0){
-      return ( <h2>Nothing on the list</h2>)
-    }else{
-    let listItems = this.props.list.map(item =>{
-      let buttonText="Remove";
-      let buttonColor ="gray";
-      if(item.id===this.state.removeId){
-        buttonText="Confirm";
-        buttonColor="red";
-      }
-      return <Table.Row key = {item.id}>
-        <Table.Cell>{item.firstname}</Table.Cell>
-        <Table.Cell>{item.lastname}</Table.Cell>
-        <Table.Cell>{item.address}</Table.Cell>
-        <Table.Cell>{item.email}</Table.Cell>
-        <Table.Cell>{item.phone}</Table.Cell>
-        <Table.Cell><Button onClick={()=> this.remove(item.id)}>{buttonText}</Button></Table.Cell>
-      </Table.Row>
-    })
+	constructor(props) {
+		super(props);
+		this.state= {
+			removeId:0,
+			editId:0,
+			firstname:"",
+			lastname:"",
+			address:"",
+			email:"",
+			phone:""
+		}
+	}
+	
+	remove = (id) => {
+		let tempId = parseInt(id,10);
+		if(this.state.removeId === 0) {
+			this.setState({
+				removeId:tempId,
+				editId:0
+			})
+			return;
+		}
+		if(tempId === this.state.removeId) {
+			this.props.remove(id);
+		}
+		this.cancel();
+	}
+	
+	edit = (id,item) => {
+		let tempId = parseInt(id,10);
+		if(this.state.editId === 0) {
+			if(!item) {
+				return;
+			}		
+			this.setState({
+				editId:tempId,
+				removeId:0,
+				firstname:item.firstname,
+				lastname:item.lastname,
+				address:item.address,
+				email:item.email,
+				phone:item.phone
+			})
+			return;
+		}
+		if(tempId === this.state.editId) {
+			let contact = {
+				id:tempId,
+				firstname:this.state.firstname,
+				lastname:this.state.lastname,
+				address:this.state.address,
+				email:this.state.email,
+				phone:this.state.phone
+			}
+			this.props.editContact(contact,id);
+		}
+		this.cancel();
+	}
+	
+	cancel = () => {
+		this.setState({
+			removeId:0,
+			editId:0,
+			firstname:"",
+			lastname:"",
+			address:"",
+			email:"",
+			phone:""
+		})
+	}
+	
+	onChange = (event) => {
+		let state = {};
+		state[event.target.name] = event.target.value
+		this.setState(state);
+	}
 
-    return(
-      <Table>
-        <Table.Header>
-          <Table.Row>
-            <Table.HeaderCell>First Name</Table.HeaderCell>
-            <Table.HeaderCell>Last Name</Table.HeaderCell>
-            <Table.HeaderCell>Address</Table.HeaderCell>
-            <Table.HeaderCell>E-mail</Table.HeaderCell>
-            <Table.HeaderCell>Phone</Table.HeaderCell>
-            <Table.HeaderCell>Remove</Table.HeaderCell>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {listItems}
-        </Table.Body>
-      </Table>
-    );
-  }
-}
+	render() {
+		if(this.props.list.length === 0) {
+			return (<h2>Nothing on the list</h2>)
+		} else {
+			let listItems = this.props.list.map(item => {
+				if(this.state.editId === 0) {
+					if(this.state.removeId === item.id) {
+						return <RemoveRow key={item.id}
+								item={item}
+								remove={this.remove}
+								cancel={this.cancel}/>
+					} else {
+						return <NormalRow key={item.id}
+								item={item}
+								remove={this.remove}
+								edit={this.edit}/>
+					}
+				} else {
+					if(this.state.editId === item.id) {
+
+						return <EditRow key={item.id}
+								item={item}
+								firstname={this.state.firstname}
+								lastname={this.state.lastname}
+								address={this.state.address}
+								email={this.state.email}
+								phone={this.state.phone}
+								save={this.edit}
+								cancel={this.cancel}
+								onChange={this.onChange}/>
+					} else {
+						return <NormalRow key={item.id}
+								item={item}
+								remove={this.remove}
+								edit={this.edit}/>
+					}
+				}
+			})
+			
+			return (
+				<Table>
+					<Table.Header>
+						<Table.Row>
+							<Table.HeaderCell>First Name</Table.HeaderCell>
+							<Table.HeaderCell>Last Name</Table.HeaderCell>
+							<Table.HeaderCell>Address</Table.HeaderCell>
+							<Table.HeaderCell>Email</Table.HeaderCell>
+							<Table.HeaderCell>Phone</Table.HeaderCell>
+							<Table.HeaderCell>Remove</Table.HeaderCell>
+							<Table.HeaderCell>Edit</Table.HeaderCell>
+						</Table.Row>
+					</Table.Header>
+					<Table.Body>
+					{listItems}
+					</Table.Body>
+				</Table>
+		
+		)
+	}
+	}
 }
