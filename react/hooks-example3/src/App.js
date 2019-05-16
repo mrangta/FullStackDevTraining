e8858c29c0835a7d62da5f18d6b635a4f2382067
Mrangta <mrangta010@gmail.com>
2019-05-16 13:04:20 +0300

    Hooks-useReducer-example

---
+++ react/hooks-example3/src/App.js
@@ -0,0 +1,54 @@
+import React, { useReducer } from 'react';
+import './App.css';
+import ShoppingList from './components/ShoppingList';
+import ShoppingForm from './components/ShoppingForm';
+
+const initialState = {
+  list: []
+}
+
+const listReducer = (state, action) => {
+  let tempList = [];
+  switch (action.type) {
+    case "ADD_TO_LIST":
+      tempList = state.list.concat(action.item);
+      return {
+        list: tempList
+      }
+    case "REMOVE_FROM_LIST":
+      tempList = state.list;
+      tempList.splice(action.index, 1);
+      return {
+        list: tempList
+      }
+    default:
+      return state;
+  }
+}
+
+function App() {
+  const [state, dispatch] = useReducer(listReducer, initialState);
+
+  const addToList = (item) => {
+    dispatch({ type: "ADD_TO_LIST", item: item })
+  }
+
+  const removeFromList = (index) => {
+    dispatch({ type: "REMOVE_FROM_LIST", index: index })
+  }
+
+  return (
+    <>
+      <div className="App">
+        <h3>Shopping Form</h3>
+        <ShoppingForm addToList={addToList} />
+        <hr />
+      </div>
+      <div>
+        <ShoppingList list={state.list} removeFromList={removeFromList} />
+      </div>
+    </>
+  );
+}
+
+export default App;
